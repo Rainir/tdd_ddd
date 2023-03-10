bb839951863308dca91c474c8e384c424f2e690b
Rainir <off_rainir@proton.me>
2023-03-10 08:22:52 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    Map<String, Integer> phoneBook = new HashMap<>();
+
+    public int add(String name, int phone) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    PhoneBook sut;
+    @BeforeEach
+    public void initEachTest() {
+        sut = new PhoneBook();
+    }
+    @BeforeEach
+    public void afterEachTest() {
+        sut = null;
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(sut.add("Nike", 354681), 1);
+    }
+
+    @Test
+    public void testAddTwo() {
+        sut.add("Nike", 354681);
+        assertEquals(sut.add("Anny", 481900), 2);
+    }
 }
